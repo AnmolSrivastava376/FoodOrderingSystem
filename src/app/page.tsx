db6016d17a116f950d82f50122a2fe6155c8e80b
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2025-04-09 14:50:55 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page.</p>
+      <p>
+        <a href="/about">Go to About Page</a>
+      </p>
+    </div>
+  );
+}
