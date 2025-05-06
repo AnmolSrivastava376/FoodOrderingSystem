c84c2ed846efda03839ea00e7decdd6c7024dd17
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2025-05-07 02:18:24 +0530

    Revert "test commit"
    This reverts commit a6fee7fee5aa30d5117cd5ac28671a18befe9684.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import {Example} from "@/components/DND/Example";
-
 export default function Home() {
   return (
     <div>
       <h1>Welcome to My Website</h1>
       <p>This is the home page.</p>
       <div className="flex gap-4">
-        <a className="border px-2 py-1 rounded-md" href="/admin">Admin</a>
-        <a className="border px-2 py-1 rounded-md" href="/kitchen">Kitchen</a>
-        <a className="border px-2 py-1 rounded-md" href="/5">Table 5</a>
+        <a className="border px-2 py-1 rounded-md" href="/admin">Go to Admin page</a>
+        <a className="border px-2 py-1 rounded-md" href="/kitchen">Go to Kitchen page</a>
       </div>
-      <Example />
     </div>
   );
 }
